17cc274e72bf9568e14e3faa2dd1ef53804194a1
lpelczar <lukepelczar@gmail.com>
2018-02-01 22:13:47 +0100

    Add code passing test checking storing planet in ship

--- src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
+++ src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
@@ -5,13 +5,23 @@ import java.lang.reflect.Array;
 public class Ship {
 
     private Location location;
+    private Planet planet;
 
     Ship(Location location) {
         this.location = location;
     }
 
+    Ship(Location location, Planet planet) {
+        this.location = location;
+        this.planet = planet;
+    }
+
     public Location getLocation() {
         return location;
+    }
+
+    public Planet getPlanet() {
+        return planet;
     }
 
     public boolean moveForward() {
