8b7ad01ef0d7535396c436262a3830c38785aabd
lpelczar <lukepelczar@gmail.com>
2018-02-01 19:56:21 +0100

    Add test checking if location is set properly:

--- src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -7,4 +7,9 @@ import static org.testng.Assert.*;
 @Test
 public class ShipSpec {
 
+    public void whenInstantiatedThenLocationIsSet() {
+        Location location = new Location(new Point(21, 86), Direction.SOUTH);
+        Ship ship = new Ship(location);
+        assertEquals(ship.getLocation(), location);
+    }
 }
