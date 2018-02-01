5448c431d03820a6bd0851c50774287c1f6769cd
lpelczar <lukepelczar@gmail.com>
2018-02-01 19:58:52 +0100

    Add passing ship instantiation test

--- src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
+++ src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
@@ -2,4 +2,13 @@ package com.packtpublishing.tddjava.ch04ship;
 
 public class Ship {
 
+    private Location location;
+
+    Ship(Location location) {
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
 }
